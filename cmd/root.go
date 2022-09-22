86daa564ee25d77316e6e7b87fa60f9152787ff8
mdb <dauren.asir@gmail.com>
2022-09-22 19:12:55 +0600

    refactor

--- cmd/root.go
+++ cmd/root.go
@@ -42,7 +42,7 @@ func Execute() {
 	docs.SwaggerInfo.Host = conf.SwagHost
 	docs.SwaggerInfo.BasePath = conf.SwagBasePath
 	docs.SwaggerInfo.Schemes = []string{conf.SwagSchema}
-	docs.SwaggerInfo.Title = "Stg service"
+	docs.SwaggerInfo.Title = "Websocket service"
 
 	// START
 
